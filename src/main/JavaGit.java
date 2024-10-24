be78f962cdb8082adf1eea8f4b83d104bd2abcc1
LUW72 <schneider.ivan337@gmail.com>
2024-10-24 15:48:47 +0200

    Komment: feladat leirasa

--- src/main/JavaGit.java
+++ src/main/JavaGit.java
@@ -3,6 +3,9 @@ package main;
 public class JavaGit {
 
     public static void main(String[] args) {
+        /*Ha hideg van, viszek kabátot, egyébként nem viszek*/
+        /* 10 fok alatt van hideg */
+
         
     }
     
