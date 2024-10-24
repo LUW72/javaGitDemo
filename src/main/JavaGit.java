6c23db5c67ebb5fe53a8277be2d04c32b3fc9b5b
LUW72 <schneider.ivan337@gmail.com>
2024-10-24 15:54:38 +0200

    Feladat megoldasa: Hagyomanyos if/else szerkezettel

--- src/main/JavaGit.java
+++ src/main/JavaGit.java
@@ -6,6 +6,16 @@ public class JavaGit {
         /*Ha hideg van, viszek kabátot, egyébként nem viszek*/
         /* 10 fok alatt van hideg */
 
+        final int HIDEG_HATAR = 10;
+        
+        int fok = 7;
+        boolean hidegVan = fok < HIDEG_HATAR;
+        String kiiras = "Nem viszek kabátot";
+        
+        if(hidegVan){
+            kiiras = "Viszek kabátot";
+        }
+        System.out.println(kiiras);
         
     }
     
